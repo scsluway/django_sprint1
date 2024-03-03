014a5f3e355ff62346dcc5729419c510c35d1703
Muhammad Saidov <saidov.m.05.01@gmail.com>
2024-03-03 08:41:54 +0300

    Made view-funcs in the blog app

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    template = 'blog/index.html'
+    return render(request, template)
+
+
+def post_detail(request, id):
+    template = 'blog/detail.html'
+    return render(request, template)
+
+
+def category_posts(request, category_slug):
+    template = 'blog/category.html'
+    return render(request, template)
